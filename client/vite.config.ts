a51dbe3aba680b79097e096057ade4d45e56eec6
Emelie-Dev <godfatheremerie@gmail.com>
2025-03-24 21:25:33 +0100

    Fix: Updated PWA Code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,22 +9,21 @@ export default defineConfig({
       registerType: 'autoUpdate', // Automatically updates service worker
       devOptions: { enabled: true }, // Enables PWA in dev mode
       manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA',
-        description:
-          'A Progressive Web App built with Vite, React, and TypeScript.',
+        name: 'Buzzer',
+        short_name: 'Buzzer',
+        description: 'A social media web application.',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone', // Runs as a standalone app
-        start_url: '/', // Ensures the app loads correctly
+        start_url: '/auth', // Ensures the app loads correctly
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/icons/icon1.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/icons/icon2.png',
             sizes: '512x512',
             type: 'image/png',
           },
