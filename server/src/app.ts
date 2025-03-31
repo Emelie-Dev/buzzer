06e6093cdd077add3b143c8b6b77df60256adac4
Emelie-Dev <godfatheremerie@gmail.com>
2025-03-31 15:25:58 +0100

    Fix: Updated backend settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -49,7 +49,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Render static files
-app.use(express.static('/app/dist/public'));
+// app.use(express.static('/app/dist/public'));
 
 // Adds security headers
 app.use(helmet());
