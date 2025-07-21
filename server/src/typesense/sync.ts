d7803d960a1d0624dd911b26d69237dc9dfd768a
Emelie-Dev <godfatheremerie@gmail.com>
2025-07-21 22:05:57 +0100

    Fix: Updated cloudinary feature

--- server/src/typesense/sync.ts
+++ server/src/typesense/sync.ts
@@ -63,6 +63,8 @@ export async function syncAllCollections() {
         : await model.find();
     const typesenseDocs = docs.map((doc: any) => transformDocument(doc));
 
+    console.log(typesenseDocs);
+
     await typesense
       .collections(schemaName)
       .documents()
