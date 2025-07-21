e4f7e31c2d4f36c03a958d0daee5a78e672ee85a
Emelie-Dev <godfatheremerie@gmail.com>
2025-07-21 18:31:58 +0100

    Fix: Updated cloudinary feature

--- server/src/worker.js
+++ server/src/worker.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { tmpdir } from 'os';
 import ffmpeg from 'fluent-ffmpeg';
 import axios from 'axios';
+import cloudinary from 'cloudinary';
 // import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
 // import ffprobeInstaller from '@ffprobe-installer/ffprobe';
 
